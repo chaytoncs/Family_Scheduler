13920c14c59d85bc42fbe4b6c5be9be40055cb43
chaytoncs <chayton99sutton@gmail.com>
2022-10-24 15:52:08 -0500

    Added Controllers and Views

---
+++ FamilyScheduler/FamilyScheduler/Controllers/ScheduleController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FamilyScheduler.Controllers
+{
+    [Route("Schedule")]
+    public class ScheduleController : Controller
+    {
+        [Route("List")]
+        public IActionResult List()
+        {
+            return View("List");
+        }
+
+        [Route("Entry")]
+        public IActionResult Entry()
+        {
+            return View("Entry");
+        }
+
+        [Route("Delete/{id}")]
+        public IActionResult Delete()
+        {
+            return View("Delete");
+        }
+    }
+}
---
+++ FamilyScheduler/FamilyScheduler/Controllers/TaskController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FamilyScheduler.Controllers
+{
+    [Route("Task")]
+    public class TaskController : Controller
+    {
+        [Route("List")]
+        public IActionResult List()
+        {
+            return View("List");
+        }
+
+        [Route("Entry")]
+        public IActionResult Entry()
+        {
+            return View("Entry");
+        }
+
+        [Route("Details/{id}")]
+        public IActionResult Details()
+        {
+            return View("Details");
+        }
+
+        [Route("Edit/{id}")]
+        public IActionResult Edit()
+        {
+            return View("Edit");
+        }
+
+        [Route("Delete/{id}")]
+        public IActionResult Delete()
+        {
+            return View("Delete");
+        }
+    }
+}
